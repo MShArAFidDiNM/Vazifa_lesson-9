520381d3988b70d98f3803d2a94e06d6ae775c21
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-05-11 09:29:22 +0500

    Home Work

---
+++ Home_work_Lesson-9/Home_work_Lesson-9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work_Lesson_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
